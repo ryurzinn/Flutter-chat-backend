de4296264d0dbd6737d08f836535e056cd154bac
ryurzinn <chinwenwencha47@gmail.com>
2023-01-21 13:46:27 -0300

    Fin seccion 8

---
+++ routes/auth.js
@@ -0,0 +1,33 @@
+//path: api/login
+
+const {Router} = require('express');
+const { check } = require('express-validator');
+
+const { crearUsuario, login, renewToken } = require('../controllers/auth');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
+
+const router = Router();
+
+
+
+router.post('/new',[
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'El password es obligatorio').not().isEmpty(),
+
+    validarCampos
+] , crearUsuario );
+
+
+
+router.post('/', [
+    check('password', 'El password es obligatorio').not().isEmpty(),
+    check('email', 'El email es obligatorio').isEmail(),
+], login);
+
+// validarJWT,
+router.get('/renew', validarJWT, renewToken);
+
+
+module.exports = router;
